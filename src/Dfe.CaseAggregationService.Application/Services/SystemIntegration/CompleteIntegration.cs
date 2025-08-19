b54ff9317d0c0ec6dc2cbe922363798db4e062f6
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2025-08-19 15:51:48 +0100

    Wip Complete integration refactor

--- src/Dfe.CaseAggregationService.Application/Services/SystemIntegration/CompleteIntegration.cs
+++ src/Dfe.CaseAggregationService.Application/Services/SystemIntegration/CompleteIntegration.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dfe.CaseAggregationService.Application.Cases.Queries.GetCasesForUser;
+using Dfe.CaseAggregationService.Application.Common.Models;
+using Dfe.CaseAggregationService.Application.Services.Builders;
+using Dfe.CaseAggregationService.Domain.Entities.Complete;
+using Dfe.CaseAggregationService.Domain.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace Dfe.CaseAggregationService.Application.Services.SystemIntegration
 {
-    internal class CompleteIntegration
+    public class CompleteIntegration(ICompleteRepository repo,
+        IGetCaseInfo<CompleteSummary> mapper,
+        ILogger<CompleteIntegration> logger)
+        : ISystemIntegration
     {
+        
+        public Task<IEnumerable<UserCaseInfo>> GetCasesForQuery(GetCasesForUserQuery query, CancellationToken cancellationToken)
+        {
+            if (query.IncludeComplete)
+            {
+                return repo.GetCompleteSummaryForUser(query.UserEmail, cancellationToken)
+                    .ContinueWith(ProcessComplete, cancellationToken);
+            }
+
+            return Task.FromResult<IEnumerable<UserCaseInfo>>(new List<UserCaseInfo>());
+        }
+
+        private IEnumerable<UserCaseInfo> ProcessComplete(Task<IEnumerable<CompleteSummary>> cases)
+        {
+            if (!cases.IsFaulted)
+            {
+                var recast = cases.Result.ToList();
+                return recast.Select(mapper.GetCaseInfo);
+            }
+            
+            if (cases.Exception is not null)
+                logger.LogError(cases.Exception, cases.Exception.Message);
+            
+            return [];
+        }
     }
 }
---
+++ src/Tests/Dfe.CaseAggregationService.Application.Tests/Services/GetCaseInfoFromCompleteSummaryTests.cs
@@ -0,0 +1,45 @@
+﻿using Dfe.CaseAggregationService.Application.Services.Builders.Dfe.CaseAggregationService.Application.Services.Builders;
+using Dfe.CaseAggregationService.Application.Services.Builders;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dfe.CaseAggregationService.Domain.Entities.Complete;
+
+namespace Dfe.CaseAggregationService.Application.Tests.Services
+{
+    public class GetCaseInfoFromCompleteSummaryTests
+    {
+        [Fact]
+        public void GivenTransferReturnCaseInfo()
+        {
+            var completeSummary = new CompleteSummary(
+                ProjectId: Guid.NewGuid(),
+                CaseType: "CompleteTransfer",
+                AcademyName: "Academy Name",
+                Urn: "123456",
+                ProposedTransferDate: new DateTime(2010, 9, 8),
+                IncomingTrust: "Incoming Trust",
+                OutgoingTrust: "Outgoing Trust",
+                LocalAuthority: "Local Authority",
+                CreatedDate: new DateTime(2010, 9, 8),
+                UpdatedDate: new DateTime(2010, 9, 9)
+            );
+            
+            var getGuidanceLinks = Substitute.For<IGetGuidanceLinks>();
+            getGuidanceLinks.GenerateLinkItems(Arg.Any<string>()).Returns([]);
+
+            var getResourcesLinks = Substitute.For<IGetResourcesLinks>();
+            getResourcesLinks.GenerateLinkItems(Arg.Any<string>()).Returns([]);
+
+            var getSystemLinks = Substitute.For<IGetSystemLinks>();
+            getSystemLinks.GetPrepareTransferTitleLink(Arg.Any<string>()).Returns("http://TitleLink");
+
+            var underTest = new GetCaseInfoFromCompleteSummary(getGuidanceLinks, getResourcesLinks, getSystemLinks);
+
+            var result = underTest.GetCaseInfo(completeSummary);
+        }
+    }
+}
